b0fd88ea68a718540c43940ba317dba2f3c595f7
kk_666 <kirankumpatla666@gmail.com>
2023-11-02 22:09:43 +0530

    Shell Scripting Automation

--- backend.sh
+++ backend.sh
@@ -1,33 +1,59 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+
+color="\e[33m"
+log_file="/tmp/expense.log"
+
+echo -e "${color} Disabling the NODEJS Default Version  \e[0m"
+dnf module disable nodejs -y  &>>$log_file
+echo $?
+
+echo -e "${color} Enabling the NodeJS v18 as requested by the Developer  \e[0m"
+dnf module enable nodejs:18 -y &>>$log_file
+echo  $?
+
+echo -e "${color} Installing NodeJS  \e[0m"
+dnf install nodejs -y &>>$log_file
+echo  $?
+
+echo -e "${color} Copying the content of Backend Service  \e[0m"
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo  $?
+
+echo -e "${color} Adding Application User  \e[0m"
+useradd expense &>>$log_file
+echo  $?
+
+echo -e "${color} Creating Directory  \e[0m"
+mkdir /app &>>$log_file
+echo  $?
+
+echo -e "${color} Delete old Application Content \e[0m"
+rm -rf /app/* &>>$log_file
+echo $?
+
+echo -e "${color} Download Application Content \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
+echo  $?
+
+echo -e "${color} Extract Application Content \e[0m"
+cd /app &>>$log_file
+unzip /tmp/backend.zip &>>$log_file
+echo  $?
 
 
-dnf install nodejs -y
+echo -e "${color} Download NodeJS Dependencies \e[0m"
+npm install &>>$log_file
+echo  $?
 
-cp backend.service /etc/systemd/system/backend.service
+echo -e "${color} Install MySQL Client to Load Schema \e[0m"
+dnf install mysql -y &>>$log_file
+echo  $?
 
+echo -e "${color} Load Schema \e[0m"
+mysql -h  mysql-dev.kgsdevops.online  -uroot -pIndian@123 < /app/schema/backend.sql &>>$log_file
+echo  $?
 
-useradd expense
-
-
-mkdir /app
-
-
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
-
-
-cd /app
-
-npm install
-
-dnf install mysql -y
-
-mysql -h  MYSQL-SERVER-IPADDRESS> -uroot -pIndian@123 < /app/schema/backend.sql
-
-systemctl daemon-reload
-
-systemctl enable  backend
-
-systemctl restart backend
+echo -e "${color} Starting Backend Services \e[0m"
+systemctl daemon-reload &>>$log_file
+systemctl enable  backend &>>$log_file
+systemctl restart backend &>>$log_file
+echo  $?
--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,17 @@
-dnf module disable mysql -y
 
+color="\e[36m"
+echo -e "${color}  Disabling MYSql  \e[0m"
+dnf module disable mysql -y
+echo $?
+
+echo -e "${color} Copying Content to mysql.repo file  \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
+echo -e "${color} Installing Mysql Community Edition  \e[0m"
 dnf install mysql-community-server -y
 
+echo -e "${color} Installing nginx  \e[0m"
 systemctl enable  mysqld
-
 systemctl start mysqld
 
 
