5a1d6954e4ba891d40b272da23c6c595c54634e3
kk_666 <kirankumpatla666@gmail.com>
2023-11-02 21:45:30 +0530

    Shell Scripting Automation

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,26 @@
-dnf install nginx -y
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+color="\e[36m"
+log_file="/tmp/expense.log
+echo -e "${color} Installing nginx  \e[0m"
 
-rm -rf /usr/share/nginx/html/*
+dnf install nginx -y  &>>/tmp/expense.log
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "${color} Copying Expense file  \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+echo -e "${color} Clean Old nginx content  \e[0m"
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
 
 
-systemctl enable  nginx
+echo -e "${color} Downloading FrontEnd Content  \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
 
-systemctl restart nginx
+
+cd /usr/share/nginx/html &>>/tmp/expense.log
+unzip /tmp/frontend.zip &>>/tmp/expense.log
+
+echo -e "${color} Starting Nginx Service  \e[0m"
+systemctl enable  nginx &>>/tmp/expense.log
+systemctl restart nginx &>>/tmp/expense.log
 
 
